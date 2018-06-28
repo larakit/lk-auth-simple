ebe75ec48edce19e4782ba34c74560220443ebab
Бердников Алексей <berdnikov_ay@bunkermedia.ru>
2018-06-28 16:58:07 +0500

    fixed

--- boot/routes.php
+++ boot/routes.php
@@ -1,17 +1,20 @@
 <?php
+
 Route::group(['middleware' => 'web'], function () {
     Route::get('login', function () {
         return view('login');
     })
          ->name('login');
     Route::post('login', function () {
+
         $login_field = config('auth-simple.login_field', 'email');
         $login       = Request::input($login_field);
         $remember    = ('on' == Request::input('remember'));
         $password    = Request::input('password');
-        $error      = '';
-        $exist = \App\User::where($login_field, $login)->count();
-        if(!$exist){
+        $error       = '';
+        $exist       = \App\User::where($login_field, $login)
+                                ->count();
+        if (!$exist) {
             $error = 'Пользователь отсутствует на проекте';
         } else {
             if (!Auth::attempt(compact([$login_field, 'password']), $remember)) {
